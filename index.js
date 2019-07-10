ff246ff1f1dc6d6f4f214c46286d7c1f07d8ba95
cedric e goddard <8ceg6@comcast.net>
2019-07-10 21:10:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-var kittens = [] //define your array here
+var kittens = ["Milo","Otis","Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(array){
+          return kittens.push("Ralph")
+}
+
+function destructivelyPrependKitten(array){
+          return kittens.unshift('Bob')
+}
+
+function destructivelyRemoveLastKitten(array){
+         return kittens.pop() 
+}
+
+function destructivelyRemoveFirstKitten(array){
+         return kittens.shift() 
+}
+
+function appenKitten()
